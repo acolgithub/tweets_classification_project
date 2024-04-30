ad143caff9cbbd30e3902266e94d42ac4f2f0a59
Andrew <andrew.colinet@alum.utoronto.ca>
2024-04-29 23:52:58 -0400

    Updating structure of app.

--- disaster_tweet_app.py
+++ disaster_tweet_app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['TESTING'] = True
 
 @app.route("/")
 def home():
-    return 'This is the home page for the app. You can see the predictions from the model by appending /predictions'
+    return render_template("home.html")
 
 @app.route('/predictions')
 def hello_world():
-    return "Testing Flask"
+    return "Hi."
