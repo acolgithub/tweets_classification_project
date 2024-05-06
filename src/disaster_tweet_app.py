5b6561c95007b294db865cf8b67af705abef826b
Andrew <andrew.colinet@alum.utoronto.ca>
2024-05-05 22:09:35 -0400

    Updating Tweets project.

--- src/disaster_tweet_app.py
+++ src/disaster_tweet_app.py
@@ -10,9 +10,18 @@ def home():
 
 @app.route("/", methods=["GET", "POST"])
 def get_user_input():
+
+    # if user enters data return prediction
     if request.method == "POST":
+
+        # get user input
         user_input = request.form.get("input")
+
+        # create response
         response = user_input.upper()
+
         return render_template("home.html", user_text_input=user_input, response_output=response)
+    
+    # if retrieving data
     elif request.method == "GET":
         return render_template("home.html")
