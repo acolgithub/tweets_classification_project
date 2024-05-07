fc737c2054775a4e5bb2ef0113687df83ea176ce
Andrew <andrew.colinet@alum.utoronto.ca>
2024-05-06 23:59:04 -0400

    Updated application with prediction.

--- tweet_app.py
+++ tweet_app.py
@@ -1,5 +1,15 @@
+from create_model.model import Make_model
+from create_model.parameters import Params
+from create_model.preprocessing import preprocess_text
+
+import numpy as np
+import torch
+import torch.nn as nn
+from typing import Union, Tuple
+from transformers import DebertaForSequenceClassification
+
+
 from flask import Flask, render_template, request
-from .fns import hello
 
 app = Flask(__name__)
 app.config['TESTING'] = True
@@ -17,10 +27,26 @@ def get_user_input():
         # get user input
         user_input = request.form.get("input")
 
-        # create response
-        response = user_input.upper()
+        # get parameters used for model
+        params = Params()
 
-        return render_template("home.html", user_text_input=user_input, response_output=response)
+        # process text
+        preprocessed_user_input = preprocess_text([user_input], params)
+
+        # get best saved model
+        model = Make_model(params)
+        model.load_state_dict(torch.load, PATH)
+        model.eval()
+
+        # create logits
+        logits = model(
+            input_ids = preprocessed_user_input[:, :params.max_len],
+            attention_mask = preprocessed_user_input[:, params.max_len:])
+        
+        # create prediction
+        prediction = torch.argmax(logits, axis=1).flatten()
+
+        return render_template("home.html", user_text_input=user_input, response_output=prediction)
     
     # if retrieving data
     elif request.method == "GET":
